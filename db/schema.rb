12b06631f7943e840137932606109115bc48af26
Andrzej Sienkiewicz <a.siennkiewicz@gmail.com>
2019-08-03 00:21:08 +0200

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,15 @@
 
 ActiveRecord::Schema.define(version: 2019_07_15_120740) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "food_items", force: :cascade do |t|
     t.string "name"
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["name", "user_id"], name: "index_food_items_on_name_and_user_id", unique: true
     t.index ["user_id"], name: "index_food_items_on_user_id"
   end
@@ -27,7 +30,7 @@ ActiveRecord::Schema.define(version: 2019_07_15_120740) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["name", "user_id"], name: "index_recipes_on_name_and_user_id", unique: true
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
@@ -40,4 +43,6 @@ ActiveRecord::Schema.define(version: 2019_07_15_120740) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "food_items", "users"
+  add_foreign_key "recipes", "users"
 end
