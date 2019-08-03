bf443d4ba639cd97a9361cc300e7c0ba894167a7
Andrzej Sienkiewicz <a.siennkiewicz@gmail.com>
2019-08-04 01:32:03 +0200

    Update JWT SECRET to use ENV SECRET_KEY_BASE

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,13 +1,13 @@
 class JsonWebToken
-  JWT_SECRET = Rails.application.secrets.secret_key_base
+  # SECRET_KEY_BASE = Rails.application.secrets.secret_key_base
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, JWT_SECRET)
+    JWT.encode(payload, ENV['SECRET_KEY_BASE'])
   end
 
   def self.decode(token)
-    decoded = JWT.decode(token, JWT_SECRET)[0]
+    decoded = JWT.decode(token, ENV['SECRET_KEY_BASE'])[0]
     HashWithIndifferentAccess.new decoded
   rescue JWT::ExpiredSignature, JWT::VerificationError => e
     raise ExceptionHandler::ExpiredSignature, e.message
